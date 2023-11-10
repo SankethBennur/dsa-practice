011d1dd7dd523127d5ade2f330ec64c529b5d8e9
sanketh-bennur-kasmo <sanketh.bennur@kasmo.co>
2023-11-10 17:04:15 +0530

    Finally got the code to work in the first try!

--- concepts/linked-list.js
+++ concepts/linked-list.js
@@ -1,8 +1,7 @@
 /*
 	Write a linked list class to perform the following
 	1. Insert node at index
-		a. if index <= 0, insert in first index
-		b. if index out of bounds, insert last
+		b. if index < 0 or out of bounds, insert last
 		c. return head
 	2. Get node at index
 		a. return node or null
@@ -22,119 +21,140 @@ class linked_list
 {
 	constructor(head)
 	{
-		this.__head = (head && head.__value)
+		this.__head = (
+				head &&
+				head.constructor &&
+				head.constructor.name
+			)
 			? head
 			: null;
 	}
 
 	__add_node_at_index = (value, index = -1) =>
 	{
-		let current_node_ = this.__head;
-		let new_node_ = new node(value);
-
 		// handle no head
-		if(!current_node_)
+		if (!this.__head)
 		{
-			this.__head = new_node_;
-			return this.__head;
+			this.__head = new node(value);
+			return (this.__head);
 		}
 
-		// handle index <= 0
-		if(index <= 0)
+		let new_node_ = new node(value);
+		
+		// handle index === 0
+		if (index === 0)
 		{
-			new_node_.__next = current_node_;
+			new_node_.__next = this.__head;
 			this.__head = new_node_;
-			return this.__head;
+			return (this.__head);
 		}
-
-		// iterate till index - 1, get previous node
-		// current_node_ = this.__get_node_at_index(index-1);
+		
+		let iterating_node_ = this.__head;
 
 		let current_index_ = -1;
 
-		while(current_node_)
+		// get previous node
+		while(iterating_node_)
 		{
+			// increment current index
 			++current_index_;
+			
+			// if current_index_ === index - 1 ; for previous node
+			if (
+				current_index_ === index-1 ||
+				!iterating_node_.__next			// last element - no next element
+			)
+				// break
+				break;		// break retaining last element
 
-			if(current_index_ === index - 1)
-				// perform insertion
-				break;
-
-			if (current_node_.__next)
-				current_node_ = current_node_.__next;
+			// iterate
+			iterating_node_ = iterating_node_.__next;
 		}
+		
+		// add after previous node ; must also handle out of bounds
+		new_node_.__next = iterating_node_.__next;
+		iterating_node_.__next = new_node_;
 
-		new_node_.__next = current_node_.__next;
-		current_node_.__next = new_node_;
 		return this.__head;
-
 	}
 
-	__get_node_at_index = (index) =>
+	__get_node_at_index = (index = -1) =>
 	{
+		/*
+			return null if index < 0  or out of bounds
+		*/
+		if (index < 0 )
+			return null;
+
 		let current_node_ = this.__head;
 		let current_index_ = -1;
 
-		if(index < 0) return null;		// negative index
-
-		while(current_node_)
+		// while (current_node_)
+		while (current_node_)
 		{
+			// ++current_index_
 			++current_index_;
 
-			if(current_index_ === index)
+			// if current_index_ === index
+			if (current_index_ === index)
+				// return current_node_
 				return current_node_;
 
-			current_node_ = current_node_.__next;
+			// iterate
+			if (current_node_.__next)
+				current_node_ = current_node_.__next;
+			else
+				break;
 		}
 
-		return null;	// out of bounds
+		return null;		// null if out of bounds
 	}
 
 	__display_linked_list = (from = 0, to = -1) =>
 	{
-		// current node <- head
+		// if to < 0, iterate till end
+		// stop only if to > 0 and current index === to
+
+		// current node <- this.__head
 		let current_node_ = this.__head;
-
-		// if not current node
-		if(!current_node_)
-			// return
-			return;
-
-		// current index <- -1
+		
+		// current_index <- -1
 		let current_index_ = -1;
+		// node_value_arr_ = []
+		let node_value_arr_ = [];
 
-		// node_value_arr <- []
-		let node_value_arr = [];
-
-		// while current node
-		while(current_node_)
+		// while (current_node_)
+		while (current_node_)
 		{
-			// increment current index
+			// ++current_index_
 			++current_index_;
-
-			// if current index === from
-			if(current_index_ >= from)
-				// insert into list node_value_arr_
-				node_value_arr.push(current_node_.__value);
 			
-			// if current index >= to
-			if(to > 0 && current_index_ >= to)
+			// if current_index_ >= from
+			if (current_index_ >= from)
+				// append to value list
+				node_value_arr_.push(current_node_.__value);
+			
+			// if current_index_ >= to
+			if(to >= 0 && current_index_ >= to)
 				// break
 				break;
 			
-			// move node; current node <- current_node.__next
-			current_node_ = current_node_.__next;
+			if (current_node_.__next)
+				current_node_ = current_node_.__next;
+			else
+				break;
 		}
-
-		console.log();
-		console.log('Linked List');
-		console.log('===========');
-		console.log(node_value_arr.join(` -> `));
-		console.log('===========');
-		console.log();
+		
+		// display values of list
+		console.log(``);
+		console.log(`Linked List`);
+		console.log(`============`);
+		console.log(node_value_arr_.join(` -> `));
+		console.log(`============`);
+		console.log(``);
 
 	}
 
 }
 
-module.exports = {linked_list};
+module.exports = { linked_list };
--- index.js
+++ index.js
@@ -20,7 +20,10 @@ IF_linked_list.display_linked_list();
 IF_linked_list.add_node_at_index(20,20);
 IF_linked_list.display_linked_list();
 
-console.log(`node at index 4 => ${IF_linked_list.get_node_at_index(4).__value}`);
+if (IF_linked_list.get_node_at_index(20))
+	console.log(`node at index 20 => ${IF_linked_list.get_node_at_index(20).__value}`);
+else
+	console.log(`node at index 20 => ${null}`);
 
 IF_linked_list.add_node_at_index(12,4);
 IF_linked_list.display_linked_list();
@@ -28,7 +31,8 @@ IF_linked_list.display_linked_list();
 IF_linked_list.add_node_at_index(9,3);
 IF_linked_list.display_linked_list();
 
-console.log(`node at index 2 => ${IF_linked_list.get_node_at_index(2).__value}`);
+if (IF_linked_list.get_node_at_index(2))
+	console.log(`node at index 2 => ${IF_linked_list.get_node_at_index(2).__value}`);
 
 // IF_linked_list.create_new_linked_list({__value: 5});
 IF_linked_list.create_new_linked_list();
@@ -36,7 +40,8 @@ IF_linked_list.create_new_linked_list();
 IF_linked_list.add_node_at_index(20);
 IF_linked_list.display_linked_list();
 
-console.log(`node at index 4 => ${IF_linked_list.get_node_at_index(4)}`);
+if (IF_linked_list.get_node_at_index(4))
+	console.log(`node at index 4 => ${IF_linked_list.get_node_at_index(4)}`);
 
 IF_linked_list.add_node_at_index(12,4);
 IF_linked_list.display_linked_list();
@@ -44,10 +49,11 @@ IF_linked_list.display_linked_list();
 IF_linked_list.add_node_at_index(9);
 IF_linked_list.display_linked_list();
 
-IF_linked_list.add_node_at_index(9,3);
+IF_linked_list.add_node_at_index(8,2);
 IF_linked_list.display_linked_list();
 
 IF_linked_list.add_node_at_index(19,3);
 IF_linked_list.display_linked_list();
 
-console.log(`node at index 2 => ${IF_linked_list.get_node_at_index(2)}`);
+if(IF_linked_list.get_node_at_index(2))
+	console.log(`node at index 2 => ${IF_linked_list.get_node_at_index(2).__value}`);
